c7b95ce70b5f385af6b8d22b7ff22b0e3941ce6e
Sidharth Gautam <navneetnishchal1420@gmail.com>
2026-06-22 07:27:24 +0530

    local db now use server db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../lib/db";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const prisma = new PrismaClient();
 
 const projectsData = [
   {
